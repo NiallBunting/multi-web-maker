9b0768083454d3db93de522381dad658af8e453c
Niall Bunting <niall.bunting@sandigital.uk>
2025-05-30 09:54:00 +0100

    Make it so you can draw back the background

--- preview/detached-window.js
+++ preview/detached-window.js
@@ -17,4 +17,95 @@ window.addEventListener('message', e => {
 	if (e.data && e.data.logs) {
 		(window.opener || window.top).postMessage(e.data, '*');
 	}
+
+	console.log(html2canvas);
 });
+
+function onHover() {
+	let iframe = document.querySelector('iframe');
+
+	if (iframe.style.display !== 'none') {
+		html2canvas(document.querySelector('iframe').contentDocument.body, {
+			x: 0,
+			y: 0,
+			width: 512,
+			height: 512
+		}).then(canvas => {
+			// Append the canvas to the body or do something with it
+			canvas.id = 'html-canvas';
+			canvas.classList.add('holder-size');
+
+			document.querySelector('iframe').style.display = 'none';
+
+			document.getElementById('canvas-holder').innerHTML = ''; // Clear previous canvas if any
+			document.getElementById('canvas-holder').appendChild(canvas);
+
+			// Draw the image to canvas
+			const img = document.getElementById('source-image');
+			const canvas2 = document.getElementById('image-canvas');
+			const ctx2 = canvas2.getContext('2d');
+			ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+
+			let canvas1 = document.getElementById('html-canvas');
+			const ctx1 = canvas1.getContext('2d');
+
+			// Get image data
+			const imgData1 = ctx1.getImageData(
+				0,
+				0,
+				canvas1.width,
+				canvas1.height
+			).data;
+			const imgData2 = ctx2.getImageData(
+				0,
+				0,
+				canvas2.width,
+				canvas2.height
+			).data;
+
+			const len = Math.min(imgData1.length, 512 * 512 * 4);
+
+			// 4. Compare pixels
+			let differentPixels = 0;
+			for (let i = 0; i < len; i += 4) {
+				const r1 = imgData1[i],
+					g1 = imgData1[i + 1],
+					b1 = imgData1[i + 2],
+					a1 = imgData1[i + 3];
+				const r2 = imgData2[i],
+					g2 = imgData2[i + 1],
+					b2 = imgData2[i + 2],
+					a2 = imgData2[i + 3];
+				//console.log('Comparing pixels', r1, g1, b1, a1, r2, g2, b2, a2);
+
+				if (r1 !== r2 || g1 !== g2 || b1 !== b2 || a1 !== a2) {
+					differentPixels++;
+				}
+			}
+			console.log(
+				'Different pixels:',
+				differentPixels,
+				canvas1.width * canvas1.height
+			);
+			const percentDiff =
+				(1 - differentPixels / (canvas1.width * canvas1.height)) * 100;
+			document.getElementById('score-holder').innerHTML =
+				`${percentDiff.toFixed(2)} %`;
+		});
+	}
+}
+
+function onMove() {
+	let canvas = document.querySelector('#html-canvas');
+	let body = document.querySelector('body');
+
+	body.addEventListener('mousemove', function (e) {
+		const rect = canvas.getBoundingClientRect();
+		const x = e.clientX - rect.left;
+		const percent = (x / rect.width) * 100;
+
+		if (percent > 1 && percent <= 100) {
+			canvas.style.clipPath = `inset(0 ${100 - percent}% 0 0)`;
+		}
+	});
+}
